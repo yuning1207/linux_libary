b8025f1752f72a07e9077a9782d6bee8c97abf25
鱼宁 <1578517699@qq.com>
2017-12-11 00:21:04 +0800

    未登录首页响应式完成

--- js/index.js
+++ js/index.js
@@ -16,19 +16,49 @@ function header_search() {
     var btn = search.getElementsByTagName("button");
     var head = document.getElementById("hea");
     var links = search.getElementsByTagName("a");
-    input.onfocus = function() {
-        input.style.width = "80%";
-        links[0].style.display = "none";
-        links[1].style.display = "none";
-        btn[0].style.marginRight = "0";
+    var windowWidth = $(window).width();
+    if (windowWidth > 425) {
+        input.onfocus = function() {
+            input.style.width = "80%";
+            links[0].style.display = "none";
+            links[1].style.display = "none";
+            btn[0].style.marginRight = "0";
+        }
+
     }
+    if (windowWidth <= 425) {
+        input.onfocus = function() {
+            input.style.width = "65%";
+            links[0].style.display = "none";
+            links[1].style.display = "none";
+            btn[0].style.marginRight = "0";
+        }
+    }
+    if (windowWidth <= 375) {
+        input.onfocus = function() {
+            input.style.width = "55%";
+            links[0].style.display = "none";
+            links[1].style.display = "none";
+            btn[0].style.marginRight = "0";
+        }
+    }
+    if (windowWidth <= 320) {
+        input.onfocus = function() {
+            input.style.width = "50%";
+            links[0].style.display = "none";
+            links[1].style.display = "none";
+            btn[0].style.marginRight = "0";
+        }
+    }
+    var timer;
     input.onblur = function() {
         input.style.width = "35%";
-        setTimeout(function() {
+        timer = setTimeout(function() {
             links[0].style.display = "inline-block";
             links[1].style.display = "inline-block";
             btn[0].style.marginRight = "20px";
         }, 200);
     }
+    clearTimeout(timer);
 }
 addLoadEvent(header_search);
