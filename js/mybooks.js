3ff506bc475847df2f530e99a27406dcd3a7077a
鱼宁 <1578517699@qq.com>
2017-12-21 20:39:35 +0800

    linux图书管理全部完成

--- js/mybooks.js
+++ js/mybooks.js
@@ -20,4 +20,31 @@ function mybooks() {
         heads[1].style.borderBottomColor = "transparent";
     }
 }
+
+function box() {
+
+    var form = document.getElementById("box");
+    var mask = document.getElementById("mask");
+    var height = document.documentElement.clientHeight || document.body.clientHeight;
+    var width = document.documentElement.clientWidth || document.body.clientWidth;
+    var Top = (height - 365) / 2;
+    var left = (width - 400) / 2;
+    form.style.top = Top + "px";
+    form.style.left = left + "px";
+    var button = document.getElementsByClassName("modify");
+    for (var i = 0, len = button.length; i < len; i++) {
+        button[i].onclick = function() {
+            mask.style.display = "block";
+            form.style.display = "block";
+            mask.style.zIndex = "1";
+        }
+        var close = document.getElementById("close");
+        close.onclick = function() {
+            mask.style.zIndex = "-1";
+            mask.style.display = "none";
+            form.style.display = "none";
+        }
+    }
+}
 addLoadEvent(mybooks);
+addLoadEvent(box);
